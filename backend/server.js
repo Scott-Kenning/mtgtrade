5540c0e64032bcf579cb9429f27132fd67a6ed05
Christian Bookout <christianmbookout@gmail.com>
2025-01-11 15:48:34 -0800

    probably working users

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use("/users", usersRoute);
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
