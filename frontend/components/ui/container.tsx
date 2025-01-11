0418329626313f9a810bc6b29ddeada44c8a5b06
Scott Kenning <skenning128@gmail.com>
2025-01-11 15:40:02 -0800

    cards UI

--- frontend/components/ui/container.tsx
+++ frontend/components/ui/container.tsx
@@ -3,9 +3,11 @@ import NavBar from "../nav-bar";
 
 const Container = ({children}: {children: ReactNode}) => {
     return (
-        <div className="flex flex-col items-center gap-y-4 px-4 md:px-8">
+        <div className="gap-y-8">
             <NavBar/>
-            {children}
+            <div className="flex flex-col max-w-6xl items-center gap-y-4 px-4 md:px-0">
+                {children}
+            </div>
         </div>
     )
 }
