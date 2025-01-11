79a5b3213f814faee546196e9b6f523d98dec815
Christian Bookout <christianmbookout@gmail.com>
2025-01-11 15:05:34 -0800

    api update

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 router.get('/search/:substring', (req, res) => {
     res.send('search for specific card based on substr');
+
+    const { substring } = req.params;
+
+    // SELECT * FROM cards WHERE name LIKE %substring%
+
+    return { cards: [] };
 });
 
 module.exports = router;
