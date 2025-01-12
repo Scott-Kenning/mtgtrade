66d92f4271a17742c9ced3205f8828077bfce9cf
Scott Kenning <skenning128@gmail.com>
2025-01-11 16:12:13 -0800

    trade base

--- frontend/app/trade/page.tsx
+++ frontend/app/trade/page.tsx
@@ -1,6 +1,42 @@
+import Container from "@/components/ui/container"
+import TradeCard from "@/components/ui/trade-card"
+
+const tradeData = [
+    {
+      user: {id: 1},
+      owned: 7,
+      wanted: 5
+    },
+    {
+      user: {id: 1},
+      owned: 7,
+      wanted: 5
+    },
+    {
+      user: {id: 1},
+      owned: 7,
+      wanted: 5
+    },
+    {
+      user: {id: 1},
+      owned: 7,
+      wanted: 5
+    },
+    {
+      user: {id: 1},
+      owned: 7,
+      wanted: 5
+    },
+  ]
+
 const Trades = () => {
     return (
-        <div></div>
+        <Container>
+            <h2 className="text-xl font-semibold">Trade Matches</h2>
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8">
+                {tradeData.map((trade, i) => <TradeCard {...trade} key={i} />)}
+            </div>
+        </Container>
     )
 }
 
--- frontend/components/ui/trade-card.tsx
+++ frontend/components/ui/trade-card.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
+
 const TradeCard = ({user, owned, wanted}: any) => {
     return (
-        <div className="flex flex-col w-full border border-neutral-600 rounded-lg p-4">
-            <p className="font-bold text-lg">Bobby McBobberson</p>
+        <div className="flex justify-between items-center w-full border border-neutral-600 rounded-lg p-4">
             <div className="flex flex-col">
-                <p>Owned: {owned}</p>
-                <p>Wanted: {wanted}</p>
+                <p className="font-bold text-lg">Bobby McBobberson</p>
+                <div className="flex flex-col">
+                    <p>Owned: {owned}</p>
+                    <p>Wanted: {wanted}</p>
+                </div>
             </div>
+            <Link href={`/trade/${user.id}`} className="bg-purple-900 font-bold px-4 py-2 rounded">Trade</Link>
         </div>
     )
 }
