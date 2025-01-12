4c2a6c49645f4f5f769099725a10ca4cef503848
Christian Bookout <christianmbookout@gmail.com>
2025-01-11 16:28:58 -0800

    owned and requested cards working

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,18 @@ const express = require('express');
 const supabase = require('../supabase');
 const router = express.Router();
 
+// helper function to get user id by email
+async function getUserId(email) {
+    let userId = await supabase.from('users').select('id').eq('email', email);
+
+    if (userId.data.length === 0) {
+        console.log(`user ${email} not found`);
+        return null;
+    }
+    console.log(`userId: ${userId.data?.map((x) => x.id)} from ${email}`);
+    return userId.data?.map((x) => x.id)[0];
+}
+
 router.post('/', (req, res) => {
     res.send('create user');
     const { email } = req.body;
@@ -13,14 +25,14 @@ router.get('/', async (req, res) => {
     const user = req.query.user;
     res.send('find users info');
     console.log(`user: ${user}`);
-
+    let userId = await getUserId(user);
     // ownedCards = SELECT cardId FROM owned_cards WHERE email = user
-    let ownedCards = await supabase.from('owned_cards').select('cardId').eq('email', user);
+    let ownedCards = await supabase.from('owned_cards').select('cardId').eq('userId', userId);
     // let ownedCards = await supabase.from('owned_cards').select('*');
     // requestedCards = SELECT cardId FROM requested_cards WHERE email = user
-    let requestedCards = await supabase.from('requested_cards').select('cardId').eq('email', user);
+    let requestedCards = await supabase.from('requested_cards').select('cardId').eq('userId', userId);
     // activeTrades = SELECT * FROM trades WHERE offeree = (SELECT userId FROM users WHERE email = user) status = 'active'
-    let activeTrades = await supabase.from('trades').select('*').eq('offeree', user).eq('status', 'active');
+    let activeTrades = await supabase.from('trades').select('*').eq('offeree', userId).eq('status', 'active');
 
     console.log(`ownedCards: ${ownedCards.data}, requestedCards: ${requestedCards.data}, activeTrades: ${activeTrades.data}`);
     return { 
@@ -82,7 +94,7 @@ router.patch('/trade', (req, res) => {
 // ownedCards
 
 // MUST HAVE
-router.put('/ownedCards', (req, res) => {
+router.put('/ownedCards', async (req, res) => {
     res.send('set list of users owned cards');
 
     const user = req.query.user;
@@ -90,8 +102,20 @@ router.put('/ownedCards', (req, res) => {
 
     let ownedCards = [];
 
+    let userId = await getUserId(user);
+
+    if (!userId) return [];
+    console.log(`userId: ${userId?.data} from ${user}`);
+
     // DELETE FROM owned_cards WHERE userId = userId
+    let del = await supabase.from('owned_card').delete().eq('user_id', userId);
+    console.log(`deleted: ${del.status} ${del.data} ${del?.error?.message}`);
     // INSERT INTO owned_cards (userId, cardId) for each card in cards
+    for (let card of cards) {
+        console.log(`Inserting card ${card.cardId} for user ${userId}`);
+        let res = await supabase.from('owned_card').insert({user_id: userId, card_id: card.cardId});
+        console.log(`inserted: ${res.status} ${res.data} ${res?.error?.message}`);
+    }
 
     return ownedCards;
 });
@@ -104,15 +128,38 @@ router.put('/ownedCards', (req, res) => {
 
 // requestedCards
 
-router.put('/requestedCards', (req, res) => {
+router.put('/requestedCards', async (req, res) => {
     res.send('add card to list of users requested cards');
     const user = req.query.user;
     const { cards } = req.body;
-    let ownedCards = [];
+    // let ownedCards = [];
 
-    // DELETE FROM requested_cards WHERE userId = userId
-    // INSERT INTO requested_cards (userId, cardId) for each card in cards
-    return ownedCards;
+    let userId = await getUserId(user);
+
+    if (!userId) return [];
+    console.log(`userId: ${userId?.data} from ${user}`);
+
+    // DELETE FROM owned_cards WHERE userId = userId
+    let del = await supabase.from('requested_card').delete().eq('user_id', userId);
+    console.log(`deleted: ${del.status} ${del.data} ${del?.error?.message}`);
+    // INSERT INTO owned_cards (userId, cardId) for each card in cards
+    for (let card of cards) {
+        console.log(`Inserting card ${card.cardId} for user ${userId}`);
+        let res = await supabase.from('requested_card').insert({user_id: userId, card_id: card.cardId});
+        console.log(`inserted: ${res.status} ${res.data} ${res?.error?.message}`);
+    }
+
+    // return requestedCards;
+
+    // let userId = await getUserId(user);
+
+    // // DELETE FROM requested_cards WHERE userId = userId
+    // await supabase.from('requested_card').delete().eq('userId', userId);
+    // // INSERT INTO requested_cards (userId, cardId) for each card in cards
+    // for (let card of cards) {
+    //     await supabase.from('requested_card').insert({userId: userId, cardId: card.cardId});
+    // }
+    // return ownedCards;
     // MUST HAVE
 });
 
--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,13 @@ const app = express();
 const cardsRoute = require("./routes/cards");
 const usersRoute = require("./routes/users");
 
+app.use(express.json());
 app.use("/cards", cardsRoute);
 app.use("/users", usersRoute);
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
