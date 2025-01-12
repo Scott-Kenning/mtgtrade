deff202da6e8927a0583054afbf70f575f516fb0
Julian France <julian123france@gmail.com>
2025-01-11 16:38:40 -0800

    CORS Moment

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const cardsRoute = require("./routes/cards");
 const usersRoute = require("./routes/users");
 
+app.use(cors());
 app.use("/cards", cardsRoute);
 app.use("/users", usersRoute);
 
 const server = http.createServer(app);
+
+// Enable CORS
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
